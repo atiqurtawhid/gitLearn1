8bda59829c23542474f327afa8cd96443f225f47
unknown <atiqurbdrahman2965@gmail.com>
2023-03-23 22:40:22 +0600

    Add name funtion

--- index.js
+++ index.js
@@ -7,3 +7,16 @@ const sum =(a,b,c,g)=>{
 sum()
 const total =sum(6,18,7,8)
 console.log(total)
+
+
+
+const name =(N)=>{
+	return N
+}
+
+const fullName = name("Atiqur Rahman ")
+console.log(fullName)
+const fullName2 = name("Tawhid Afridi ")
+console.log(fullName2)
+const fullName3 = name("Doly Akter ")
+console.log(fullName3)
